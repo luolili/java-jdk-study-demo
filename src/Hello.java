a1a82707f9f7311ab4bf670ec96a48f22afb7cad
luoli <2084267015@qq.com>
2019-05-19 22:43:37 +0800

    hello

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");// for first commit
         System.out.println("write some edits");//for second commit
+        System.out.println("see how many time you commit on github");
     }
 }
