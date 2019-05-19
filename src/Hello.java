ae65fb0398683dd2d4d51e65b90a43fcbb86170b
luoli <2084267015@qq.com>
2019-05-19 22:27:04 +0800

    hello

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello");// for commit
+        System.out.println("write some edits");
     }
 }
