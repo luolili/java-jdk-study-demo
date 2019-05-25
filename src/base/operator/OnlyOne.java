28b5f37e4bd383d3582ebe0849f789f80696571f
luoli <2084267015@qq.com>
2019-05-25 20:39:11 +0800

    operator use

--- src/base/operator/OnlyOne.java
+++ src/base/operator/OnlyOne.java
@@ -2,6 +2,7 @@ package base.operator;
 
 /**
  * find the single one with ^ operator
+ * ^: same -> 0, diffrent -> 1.
  */
 public class OnlyOne {
     public static void main(String[] args) {
@@ -9,6 +10,15 @@ public class OnlyOne {
         int[] nums = {1, 2, 2, 3, 3};
         singleNumber(nums);
         singleChar("hello", "heyllo");
+        System.out.println(3 >>> 1);//1
+        System.out.println(3 >> 1);//1
+        System.out.println(-3 >>> 1);//2147483646
+        System.out.println(-3 >> 1);//-2
+        System.out.println("---------");
+        //System.out.println(3<<<1);//err
+        System.out.println(3 << 1);//6
+        System.out.println(3 << 2);//12
+
     }
 
     static int singleNumber(int[] nums) {
