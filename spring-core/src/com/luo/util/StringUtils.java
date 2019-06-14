42b4a2265086aa235adac359449ced2c1e764163
Administrator <2084267015@qq.com>
2019-06-14 21:59:21 +0800

    trimLeadingWhiteSpace

--- spring-core/src/com/luo/util/StringUtils.java
+++ spring-core/src/com/luo/util/StringUtils.java
@@ -155,4 +155,22 @@ public abstract class StringUtils {
     }
 
 
+    /**
+     * use str.regionMarches to ignore case
+     *
+     * @param str
+     * @param prefix
+     * @return
+     */
+    public static boolean startsWithIgnoreCase(String str, String prefix) {
+        return (str != null && prefix != null && str.length() >= prefix.length()
+                && str.regionMatches(true, 0, prefix, 0, prefix.length()));
+    }
+
+    public static boolean endsWithIgnoreCase(String str, String suffix) {
+        return (str != null && suffix != null && str.length() >= suffix.length()
+                && str.regionMatches(true, str.length() - suffix.length(), suffix, 0, suffix.length()));
+    }
+
+
 }
