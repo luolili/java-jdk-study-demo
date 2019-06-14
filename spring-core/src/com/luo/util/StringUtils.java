2907e1fc7fe73ad8dd22537c4b46f8ec32db76d2
Administrator <2084267015@qq.com>
2019-06-14 21:20:05 +0800

    hasText

--- spring-core/src/com/luo/util/StringUtils.java
+++ spring-core/src/com/luo/util/StringUtils.java
@@ -49,13 +49,43 @@ public abstract class StringUtils {
 
 
     /**
-     * has content in str
+     * has content in str:notice:it takes out the two conditions:str!=null and str.length()>0
+     * do not put the two conditions into the containsText method
      *
      * @param str
      * @return
      */
     public static boolean hasText(CharSequence str) {
         return (str != null && str.length() > 0 && containsText(str));
+    }
+
+    public static boolean hasText(String str) {
+        return (str != null && !str.isEmpty() && containsText(str));
+    }
+
+    public static boolean containsWhiteSpace(CharSequence str) {
+        //no white spacewhen str len is 0 or str is null
+        if (!hasLength(str)) {
+            return false;
+        }
+
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            if (Character.isWhitespace(str.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * convert String to CharSequence,then invoke containsWhiteSpace of CharSequence
+     *
+     * @param str
+     * @return
+     */
+    public static boolean containsWhiteSpace(String str) {
+        return containsWhiteSpace((CharSequence) str);
     }
 
 
