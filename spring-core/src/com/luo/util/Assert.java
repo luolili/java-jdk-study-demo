19fdf67d1b3a64d193bd5549d33e1ebbfec5c07a
Administrator <2084267015@qq.com>
2019-06-18 19:21:01 +0800

    Assert

--- spring-core/src/com/luo/util/Assert.java
+++ spring-core/src/com/luo/util/Assert.java
@@ -35,18 +35,61 @@ public abstract class Assert {
         }
     }
 
-    // arg is null
+    //  must be null. occur ex when arg is not null
     public static void isNull(Object object, String message) {
-        if (object == null) {
+        if (object != null) {
             throw new IllegalArgumentException(message);
         }
     }
 
     public static void isNull(Object object, Supplier<String> messageSupplier) {
+        if (object != null) {
+            throw new IllegalArgumentException(nullSafeGet(messageSupplier));
+        }
+    }
+
+    //must be not null
+    public static void notNull(Object object, String message) {
+        if (object == null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void notNull(Object object, Supplier<String> messageSupplier) {
         if (object == null) {
             throw new IllegalArgumentException(nullSafeGet(messageSupplier));
         }
     }
+
+
+    public static void hasLength(String text, String message) {
+        //-1 no length
+        if (!StringUtils.hasLength(text)) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void hasLength(String text, Supplier<String> messageSupplier) {
+        //-1 no length
+        if (!StringUtils.hasLength(text)) {
+            throw new IllegalArgumentException(nullSafeGet(messageSupplier));
+        }
+    }
+
+    public static void hasText(String text, String message) {
+        //-1 no length
+        if (!StringUtils.hasText(text)) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void hasText(String text, Supplier<String> messageSupplier) {
+        //-1 no length
+        if (!StringUtils.hasText(text)) {
+            throw new IllegalArgumentException(nullSafeGet(messageSupplier));
+        }
+    }
+
 
     //get string by interface Supplier
     public static String nullSafeGet(Supplier<String> messageSupplier) {
