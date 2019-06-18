790d85950250b5203284fefb73dadb3a4bc5f4ca
Administrator <2084267015@qq.com>
2019-06-18 20:14:31 +0800

    isInsanceOf

--- spring-core/src/com/luo/util/Assert.java
+++ spring-core/src/com/luo/util/Assert.java
@@ -1,5 +1,7 @@
 package com.luo.util;
 
+import java.util.Collection;
+import java.util.Map;
 import java.util.function.Supplier;
 
 /**
@@ -90,7 +92,112 @@ public abstract class Assert {
         }
     }
 
+    public static void doesNotContain(String textToSearch, String substring, String message) {
+        if (StringUtils.hasLength(textToSearch) && StringUtils.hasLength(substring) &&
+                textToSearch.contains(substring)) {
+            throw new IllegalArgumentException(message);
+        }
+    }
 
+    public static void doesNotContain(String textToSearch, String substring, Supplier<String> messageSupplier) {
+        if (StringUtils.hasLength(textToSearch) && StringUtils.hasLength(substring) &&
+                textToSearch.contains(substring)) {
+            throw new IllegalArgumentException(nullSafeGet(messageSupplier));
+        }
+    }
+
+    //array
+    public static void notEmpty(Object[] array, String message) {
+        if (ObjectUtils.isEmpty(array)) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void notEmpty(Object[] array, Supplier<String> messageSupplier) {
+        if (ObjectUtils.isEmpty(array)) {
+            throw new IllegalArgumentException(nullSafeGet(messageSupplier));
+        }
+    }
+
+    public static void notEmpty(Collection<?> collection, String message) {
+        if (CollectionUtils.isEmpty(collection)) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    //for colection
+    public static void notEmpty(Collection<?> collection, Supplier<String> messageSupplier) {
+        if (CollectionUtils.isEmpty(collection)) {
+            throw new IllegalArgumentException(nullSafeGet(messageSupplier));
+        }
+    }
+
+
+    public static void notEmpty(Map<?, ?> map, String message) {
+        if (CollectionUtils.isEmpty(map)) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    //for map
+    public static void notEmpty(Map<?, ?> map, Supplier<String> messageSupplier) {
+        if (CollectionUtils.isEmpty(map)) {
+            throw new IllegalArgumentException(nullSafeGet(messageSupplier));
+        }
+    }
+
+    public static void isInstanceOf(Class<?> type, Object obj, String message) {
+        //-1 use notNull()
+        notNull(type, "type to check against must not be null");
+        if (!type.isInstance(obj)) {
+            instanceCheckFailed(type, obj, message);
+        }
+    }
+
+    //for type
+    public static void isInstanceOf(Class<?> type, Object obj, Supplier<String> messageSupplier) {
+        //-1 use notNull()
+        notNull(type, "type to check against must not be null");
+        if (!type.isInstance(obj)) {
+            instanceCheckFailed(type, obj, nullSafeGet(messageSupplier));
+        }
+    }
+
+
+    public static void instanceCheckFailed(Class<?> type, Object obj, String msg) {
+
+        //-1 get the className
+        String className = (obj != null ? obj.getClass().getName() : "null");
+        //-2 prepare the result
+        String result = "";
+        //-3
+        boolean defaultMessage = true;
+
+        //-4
+        if (StringUtils.hasLength(msg)) {
+            if (endsWithSeparator(msg)) {
+                result = msg + " ";
+            } else {
+                result = messageWithTypeName(msg, className);
+                defaultMessage = false;
+            }
+        }
+
+        if (defaultMessage) {
+            result = result + ("Object of class [" + className + "] must be an instance of " + type);
+        }
+
+        throw new IllegalArgumentException(result);
+    }
+
+    public static boolean endsWithSeparator(String msg) {
+        return (msg.endsWith(":") || msg.endsWith(",") || msg.endsWith(".") || msg.endsWith(";"));
+    }
+
+    //add the separator between msg and typeName/className
+    public static String messageWithTypeName(String msg, Object typeName) {
+        return msg + (msg.endsWith(" ") ? "" : ":") + typeName;
+    }
     //get string by interface Supplier
     public static String nullSafeGet(Supplier<String> messageSupplier) {
         return (messageSupplier != null) ? messageSupplier.get() : null;
