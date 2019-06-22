063622f30f7c0c3e0847644bea87d4c98a3b2c88
Administrator <2084267015@qq.com>
2019-06-22 13:28:11 +0800

    iv

--- src/base/iv/ObjConversion.java
+++ src/base/iv/ObjConversion.java
@@ -9,6 +9,14 @@ public class ObjConversion {
         //
         Object o = (Object) new Foo();
         Foo foo = (Foo) o;
-        System.out.println("m: " + foo.m);//2
+        //System.out.println("m: " + foo.m);//2
+        for (int i = 0; i <= 22; ) {
+            if (i < 10) {
+                int j = 2 + i;
+                i++;
+
+                System.out.println(i + "," + j);
+            }
+        }
     }
 }
