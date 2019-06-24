aa86d65b5e97d7da5c204915b022d52f6de9a5b4
Administrator <2084267015@qq.com>
2019-06-24 19:24:27 +0800

    singleton supplier

--- spring-core/src/com/luo/function/SingletonSupplier.java
+++ spring-core/src/com/luo/function/SingletonSupplier.java
@@ -1,6 +1,7 @@
 package com.luo.function;
 
 import com.luo.lang.Nullable;
+import com.luo.util.Assert;
 
 import java.util.function.Supplier;
 
@@ -35,6 +36,11 @@ public class SingletonSupplier<T> implements Supplier<T> {
         this.singletonInstance = singletonInstance;
     }
 
+    public SingletonSupplier(Supplier<? extends T> instanceSupplier) {
+        this.instanceSupplier = instanceSupplier;
+        this.defaultSupplier = null;
+    }
+
     @Override
     @Nullable
     public T get() {
@@ -60,5 +66,33 @@ public class SingletonSupplier<T> implements Supplier<T> {
         return instance;
     }
 
-    
+
+    //the returned instance is not null
+    public T obtain() {
+        T instance = get();
+        Assert.state(instance != null, "No instance from supplier");
+        return instance;
+    }
+
+    //of way to build the class and param is not null
+    public static <T> SingletonSupplier<T> of(T instance) {
+        return new SingletonSupplier<>(instance);
+    }
+
+    public static <T> SingletonSupplier<T> of(Supplier<T> supplier) {
+
+        return new SingletonSupplier<>(supplier);
+    }
+
+    public static <T> SingletonSupplier<T> ofNullable(Supplier<T> supplier) {
+
+        return (supplier != null ? new SingletonSupplier<>(supplier) : null);
+    }
+
+    //if param instance is null
+    public static <T> SingletonSupplier<T> ofNullable(T instance) {
+        return (instance != null ? new SingletonSupplier<>(instance) : null);
+    }
+
+
 }
