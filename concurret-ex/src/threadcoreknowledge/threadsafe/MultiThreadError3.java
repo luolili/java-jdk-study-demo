955e4ce84ed0ba7690021eeb41bd3900487f1f3f
Administrator <2084267015@qq.com>
2019-09-08 20:03:14 +0800

    thread-safe

--- concurret-ex/src/threadcoreknowledge/threadsafe/MultiThreadError3.java
+++ concurret-ex/src/threadcoreknowledge/threadsafe/MultiThreadError3.java
@@ -24,12 +24,22 @@ public class MultiThreadError3 {
         return states;
     }
 
+    //解决方法：副本代替states
+    public Map<String, String> getStatesImprove() {
+        return new HashMap<>(states);
+    }
+
     public static void main(String[] args) {
         MultiThreadError3 multiThreadError3 = new MultiThreadError3();
         Map<String, String> states = multiThreadError3.getStates();
-        System.out.println(states.get("1"));
+       /* System.out.println(states.get("1"));
         states.remove("1");
-        System.out.println(states.get("1"));//null
+        System.out.println(states.get("1"));//null*/
+        Map<String, String> states2 = multiThreadError3.getStatesImprove();
+        System.out.println(states2.get("1"));
+        states2.remove("1");
+        System.out.println(states2.get("1"));
+        System.out.println(states.get("1"));
 
 
     }
