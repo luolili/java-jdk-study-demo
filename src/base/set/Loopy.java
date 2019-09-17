74bb55823ffd99c7b425a70bbb222e541f7f2539
Administrator <2084267015@qq.com>
2019-09-17 20:14:39 +0800

    thread-safe

--- src/base/set/Loopy.java
+++ src/base/set/Loopy.java
@@ -2,10 +2,11 @@ package base.set;
 
 public class Loopy {
     public static void main(String[] args) {
-        int start = Integer.MAX_VALUE - 100;
+        int start = Integer.MAX_VALUE - 2;
         int end = Integer.MAX_VALUE;
         int count = 0;
-        for (int i = start; i < end; i++) {
+        System.out.println(end + 1);//-2147483648
+        for (int i = start; i < end + 1; i++) {
             count++;
         }
         System.out.println(count);//100
