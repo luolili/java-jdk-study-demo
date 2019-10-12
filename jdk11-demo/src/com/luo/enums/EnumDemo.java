78f54cf32e1af2d8edb7f55e4e3698e4a1c227bf
Administrator <2084267015@qq.com>
2019-10-12 20:59:34 +0800

    deadlock

---
+++ jdk11-demo/src/com/luo/enums/EnumDemo.java
@@ -0,0 +1,31 @@
+package com.luo.enums;
+
+public enum EnumDemo {
+
+    MALE("male") {
+        @Override
+        public void tellSex() {
+            System.out.println("i like man");
+        }
+    },
+    FEMALE("female") {
+        @Override
+        public void tellSex() {
+            System.out.println("i like girl");
+        }
+    },
+    ;
+    private String sex;
+
+    //构造方法默认private
+    EnumDemo(String sex) {
+        this.sex = sex;
+    }
+
+    //抽象方法必须被实现
+    public abstract void tellSex();
+
+    public static void main(String[] args) {
+
+    }
+}
