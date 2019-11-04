dde4a45dad7321803da0454090e1ac18b546c8ff
Administrator <2084267015@qq.com>
2019-11-04 21:12:38 +0800

    pm

--- src/base/iv/TernaryOpTest.java
+++ src/base/iv/TernaryOpTest.java
@@ -3,10 +3,12 @@ package base.iv;
 /**
  * 三木运算符
  */
+@SuppressWarnings("uncheck")
 public class TernaryOpTest {
     public static void main(String[] args) {
 
         System.out.println(1 == 2 ? 'a' : 102);//f
+        System.out.println((int) 'f');//102
         System.out.println((1 == 2) ? 'a' : 102);//f
         System.out.println((1.1f == 2.2f) ? 'a' : 102);//f
         System.out.println((1.1d == 2.2d) ? 'a' : 102);//f
@@ -20,5 +22,7 @@ public class TernaryOpTest {
         short sh = 1;
         System.out.println(1 == 1 ? sh : 102);//1
 
+        System.out.println((int) 'a');//97
     }
+
 }
