042e4c7aa7c886dffae471d16e70d3b5f0b8a32c
Administrator <2084267015@qq.com>
2019-12-07 15:52:16 +0800

    pm

---
+++ jdk11-demo/src/com/luo/reactive/stream/FlowDemo.java
@@ -0,0 +1,58 @@
+package com.luo.reactive.stream;
+
+import java.util.concurrent.Flow;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+import java.util.concurrent.SubmissionPublisher;
+
+public class FlowDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        //发布人
+        SubmissionPublisher<Integer> publisher = new SubmissionPublisher<>();
+
+        Subscriber<Integer> subscriber = new Subscriber<Integer>() {
+            private Subscription subscription;
+
+            @Override
+            public void onSubscribe(Flow.Subscription subscription) {
+                this.subscription = subscription;
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onNext(Integer item) {
+                System.out.println("accept:" + item);
+                try {
+                    Thread.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("end");
+            }
+        };
+
+        publisher.subscribe(subscriber);
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("out:" + i);
+            publisher.submit(i);
+
+        }
+        //正式env 在finally 执行他
+        publisher.close();
+        Thread.currentThread().join(100);
+        System.out.println();
+
+    }
+}
