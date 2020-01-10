45a705fe2c9336205722f7440c11c1b3a602e178
Administrator <2084267015@qq.com>
2020-01-10 21:17:28 +0800

    pm

---
+++ src/base/string/StringToByte.java
@@ -0,0 +1,10 @@
+package base.string;
+
+public class StringToByte {
+
+    public static void main(String[] args) {
+        Byte a = Byte.valueOf("2");
+        //true
+        System.out.println(a == 2);
+    }
+}
