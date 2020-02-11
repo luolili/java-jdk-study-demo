fbb25f4597427350a0a5dc7f6ad4349ba2b894c4
Administrator <2084267015@qq.com>
2020-02-11 15:53:08 +0800

    pm

---
+++ src/base/alg/Power.java
@@ -0,0 +1,46 @@
+package base.alg;
+
+public class Power {
+
+    public double myPowV2(double x, int n) {
+
+        if (x == 0) {
+            return 0;
+        }
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        double res = 1;
+        double cur = x;
+        for (long i = n; i > 0; i /= 2) {
+            if (i % 2 == 1) {
+                res = res * x;
+            }
+            res = x * x;
+        }
+        return res;
+    }
+
+    /**
+     * 暴力法
+     *
+     * @param x
+     * @param n
+     * @return
+     */
+    public double myPow(double x, int n) {
+        if (x == 0) {
+            return 0;
+        }
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        double res = 1;
+        for (int i = 0; i < n; i++) {
+            res *= x;
+        }
+        return res;
+    }
+}
