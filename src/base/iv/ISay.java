589570f3dd68aea6b12f674cf9d25efecc875b6e
Administrator <2084267015@qq.com>
2020-05-16 21:31:07 +0800

    pm

--- src/base/iv/ISay.java
+++ src/base/iv/ISay.java
@@ -1,7 +1,12 @@
 package base.iv;
 
 public interface ISay {
+    //private  int i=0;
+    //protected   int i=0;
+    final int i = 0;
     void say();
 
     void say1();
+
+    void say2();
 }
---
+++ src/base/iv/aiqiyi/MathTest.java
@@ -0,0 +1,10 @@
+package base.iv.aiqiyi;
+
+public class MathTest {
+
+    public static void main(String[] args) {
+        //12
+        System.out.println(Math.round(11.5));
+        System.out.println(Math.round(-11.5));
+    }
+}
